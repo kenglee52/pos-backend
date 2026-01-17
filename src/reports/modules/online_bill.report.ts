9f787175ff390ede84646799e938b40baac1fd65
kenglee52 < Gitcl>
2026-01-17 18:46:08 +0700

    first commit'

--- src/reports/modules/online_bill.report.ts
+++ src/reports/modules/online_bill.report.ts
@@ -4,7 +4,7 @@ import { Request, Response } from "express";
 export const getOnlineBillByCustomerId = async(req: Request, res: Response) =>{
          const id = req.params.id;
          try {
-            const [rows] = await db.query("SELECT * FROM online_bill WHERE customerID = ?", [id]);
+            const [rows] = await db.query("SELECT * FROM online_bill WHERE customerID = ? ORDER BY onlineBillID DESC", [id]);
             res.status(200).json(rows); 
          } catch (error) {
                   console.log(error);
